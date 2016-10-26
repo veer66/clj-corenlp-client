d595169c7b42a62c478f2aabf0f94c7302f45119
Vee Satayamas <vsatayamas@gmail.com>
2016-10-26 19:22:55 +0700

    usable

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "https://gitlab.com/veer66/clj-corenlp-client"
   :license {:name "LGPL v3"
             :url "http://www.gnu.org/licenses/lgpl.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.3.0"]
+                 [com.cemerick/url "0.1.1"]
+                 [org.clojure/data.json "0.2.6"]])
--- src/clj_corenlp_client/core.clj
+++ src/clj_corenlp_client/core.clj
@@ -1,6 +1,40 @@
-(ns clj-corenlp-client.core)
+(ns clj-corenlp-client.core
+  (:require [clj-http.client :as client]
+            [cemerick.url :as cemerick-url]
+            [clojure.data.json :as json]
+            [clojure.string :as str])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create-query-string [annotators]
+  (let [properties {:annotators (str/join "," annotators)
+                    :outputFormat "json"}]
+    {:properties (json/write-str properties)}))
+
+(defn create-url
+  [host port annotators]
+  (-> (cemerick.url.URL. "http"
+                         nil
+                         nil
+                         (if host host "localhost")
+                         (if port port 9000)
+                         "/"
+                         (create-query-string annotators)
+                         nil)
+       str))
+
+(defn annotate [text annotators & [host port]]
+  (let [response (client/post (create-url host port annotators)
+                              {:body text})]
+    (when (not= (:status response) 200)
+      (throw (Exception. "Annotating Error")))
+    (json/read-str (:body response)
+                   :key-fn keyword)))
+
+(defn tokenize [text & [host port]]
+  (annotate text ["tokenize" "ssplit"] host port))
+
+(defn tag-pos [text & [host port]]
+  (annotate text ["tokenize" "ssplit" "pos"] host port))
+
+(defn dep-parse [text & [host port]]
+  (annotate text ["tokenize" "ssplit" "pos" "depparse"] host port))
