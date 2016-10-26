ebf1b0035281bbcd855a03219dfd86af06eeaac9
Vee Satayamas <vsatayamas@gmail.com>
2016-10-26 15:53:38 +0700

    Add project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-corenlp-client "0.1.0"
   :description "Unofficial Stanford CoreNLP client written in Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://gitlab.com/veer66/clj-corenlp-client"
   :license {:name "LGPL v3"
             :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
